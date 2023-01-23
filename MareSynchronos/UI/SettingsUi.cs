be9f49339dcc24d44dc248f06e157281f44c7d18
rootdarkarchon <root.darkarchon@outlook.com>
2023-01-23 13:02:12 +0100

    make note yellow

--- MareSynchronos/UI/SettingsUi.cs
+++ MareSynchronos/UI/SettingsUi.cs
@@ -751,7 +751,7 @@ public class SettingsUi : Window, IDisposable
                         });
                     });
                 }
-                UiShared.TextWrapped("Note: For best results make sure you have everything you want to be shared equipped and redraw your character before exporting.");
+                UiShared.ColorTextWrapped("Note: For best results make sure you have everything you want to be shared equipped and redraw your character before exporting.", ImGuiColors.DalamudYellow);
             }
             else
             {
