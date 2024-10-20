f50da9c87c9f5afd7b821d4a4b477b0993a7abc6
Stanley Dimant <root.darkarchon@outlook.com>
2024-10-20 21:28:20 +0200

    save on uid adding

--- MareSynchronos/UI/SettingsUi.cs
+++ MareSynchronos/UI/SettingsUi.cs
@@ -1494,6 +1494,7 @@ public class SettingsUi : WindowMediatorSubscriberBase
                 if (!_playerPerformanceConfigService.Current.UIDsToIgnore.Contains(_uidToAddForIgnore, StringComparer.Ordinal))
                 {
                     _playerPerformanceConfigService.Current.UIDsToIgnore.Add(_uidToAddForIgnore);
+                    _playerPerformanceConfigService.Save();
                 }
                 _uidToAddForIgnore = string.Empty;
             }
