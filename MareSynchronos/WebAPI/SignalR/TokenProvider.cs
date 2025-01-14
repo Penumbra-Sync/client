860e074d538953820235f09f2c5087de1b8dcce0
Stanley Dimant <root.darkarchon@outlook.com>
2025-01-15 00:37:21 +0100

    add fucking text that mare's main ui is opened with /mare

--- MareSynchronos/WebAPI/SignalR/TokenProvider.cs
+++ MareSynchronos/WebAPI/SignalR/TokenProvider.cs
@@ -118,7 +118,7 @@ public sealed class TokenProvider : IDisposable, IMediatorSubscriber
                     Mediator.Publish(new NotificationMessage("Error refreshing token", "Your authentication token could not be renewed. Try reconnecting to Mare manually.",
                     NotificationType.Error));
                 else
-                    Mediator.Publish(new NotificationMessage("Error generating token", "Your authentication token could not be generated. Check Mares Main UI to see the error message.",
+                    Mediator.Publish(new NotificationMessage("Error generating token", "Your authentication token could not be generated. Check Mares Main UI (/mare in chat) to see the error message.",
                     NotificationType.Error));
                 Mediator.Publish(new DisconnectedMessage());
                 throw new MareAuthFailureException(response);
