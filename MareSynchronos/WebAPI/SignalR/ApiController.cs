46f68b55e90bc99c650d56cdedba90c5f3f9c5b2
Stanley Dimant <root.darkarchon@outlook.com>
2025-02-19 11:50:06 +0100

    add tt msg

--- MareSynchronos/WebAPI/SignalR/ApiController.cs
+++ MareSynchronos/WebAPI/SignalR/ApiController.cs
@@ -261,9 +261,10 @@ public sealed partial class ApiController : DisposableMediatorSubscriberBase, IM
                     Logger.LogError("Detected modified game files on connection");
                     if (!_mareConfigService.Current.DebugStopWhining)
                         Mediator.Publish(new NotificationMessage("Modified Game Files detected",
-                            "Dalamud is reporting your FFXIV installation has modified game files. Mare Synchronos, Penumbra, and some other plugins assume your FFXIV installation " +
-                            "is unmodified in order to work. Synchronization with pairs/shells can break because of this. Exit the game, open XIVLauncher, click the arrow next to Log " +
-                            "In and select 'repair game files' to resolve this issue. Plugin configurations will remain, as will mods enabled in Penumbra.",
+                            "Dalamud is reporting your FFXIV installation has modified game files. Any mods installed through TexTools will produce this message. " +
+                            "Mare Synchronos, Penumbra, and some other plugins assume your FFXIV installation is unmodified in order to work. " +
+                            "Synchronization with pairs/shells can break because of this. Exit the game, open XIVLauncher, click the arrow next to Log In" +
+                            "and select 'repair game files' to resolve this issue. Aftwards, do not install any mods with TexTools. Your plugin configurations will remain, as will mods enabled in Penumbra.",
                             NotificationType.Error, TimeSpan.FromSeconds(15)));
                 }
 
